2281856f9d3be992ccb5cef36be83feae15d65e2
Ivanov Pete <superpete2005@gmail.com>
2026-06-14 19:29:06 +0300

    -

--- kurs_backend/leads/admin.py
+++ kurs_backend/leads/admin.py
@@ -4,14 +4,21 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('phone', 'login', 'yandex_id', 'age', 'start_english_level', 'group',
-                    'payments', 'attendance', 'telegram', 'vk', 'yandex', 'created_at')
-    list_filter = ('created_at', 'start_english_level', 'age', 'group')
-    search_fields = ('phone', 'login', 'yandex_id',
-                     'telegram', 'vk', 'yandex', 'group')
+    list_display = ('phone', 'login', 'yandex_id', 'email', 'first_name', 'last_name',
+                    'sex', 'birthday', 'avatar_url', 'age', 'start_english_level',
+                    'group', 'payments', 'attendance', 'telegram', 'vk', 'yandex',
+                    'free_lessons', 'created_at')
+    list_filter = ('created_at', 'start_english_level', 'age', 'group', 'sex')
+    search_fields = ('phone', 'login', 'yandex_id', 'email', 'first_name',
+                     'last_name', 'telegram', 'vk', 'yandex', 'group')
     fieldsets = (
+        ('Данные от Яндекса', {
+            'fields': ('yandex_id', 'login', 'email', 'first_name', 'last_name',
+                       'sex', 'birthday', 'avatar_url'),
+            'description': 'Информация, полученная от Яндекса при входе'
+        }),
         ('Основная информация', {
-            'fields': ('phone', 'login', 'yandex_id', 'age', 'start_english_level')
+            'fields': ('phone', 'age', 'start_english_level', 'free_lessons')
         }),
         ('Соцсети для связи', {
             'fields': ('telegram', 'vk', 'yandex'),
