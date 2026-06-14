2cc1de5244b37800513ee00ed97ddb6f9cd79233
Ivanov Pete <superpete2005@gmail.com>
2026-06-14 16:06:20 +0300

    -

--- kurs_backend/leads/admin.py
+++ kurs_backend/leads/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-from .models import Lead
+from .models import User
 
 
-@admin.register(Lead)
-class LeadAdmin(admin.ModelAdmin):
-    list_display = ('phone', 'age', 'start_english_level', 'group',
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'login', 'yandex_id', 'age', 'start_english_level', 'group',
                     'payments', 'attendance', 'telegram', 'vk', 'yandex', 'created_at')
     list_filter = ('created_at', 'start_english_level', 'age', 'group')
-    search_fields = ('phone', 'telegram', 'vk', 'yandex', 'group')
+    search_fields = ('phone', 'login', 'yandex_id',
+                     'telegram', 'vk', 'yandex', 'group')
     fieldsets = (
         ('Основная информация', {
-            'fields': ('phone', 'age', 'start_english_level')
+            'fields': ('phone', 'login', 'yandex_id', 'age', 'start_english_level')
         }),
         ('Соцсети для связи', {
             'fields': ('telegram', 'vk', 'yandex'),
